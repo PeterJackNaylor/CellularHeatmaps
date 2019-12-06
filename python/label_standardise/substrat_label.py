7b6d9843d846c977fd425876f919be2be0ce49d6
pnaylor <peter.naylor@mines-paristech.fr>
2019-12-06 20:43:49 +0100

    thalassa fix

--- python/label_standardise/substrat_label.py
+++ python/label_standardise/substrat_label.py
@@ -55,6 +55,7 @@ def split(stri):
 
 
 def main():
+    import pdb; pdb.set_trace()
     options = get_options()
     table = load_custom_xlsx(options.input_table)
     which_tiff_exist = [split(f) for f in glob(options.folder_to_check + '/*.tif')]
