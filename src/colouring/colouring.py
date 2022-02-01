ebd1a10bed09c24cd7e751937a9a157532e3b409
PeterJackNaylor <peter.jack.naylor@gmail.com>
2022-02-01 10:28:47 +0900

    parallel colouring

--- src/colouring/colouring.py
+++ src/colouring/colouring.py
@@ -4,6 +4,8 @@ sys.path.append('..')
 import os
 from os.path import join
 
+from joblib import Parallel, delayed
+
 import numpy as np
 from glob import glob
 import h5py
@@ -106,6 +108,8 @@ def main():
     parser.add_option("-s", "--no_samples",
                   action="store_false", dest="samples", default=True,
                   help="If to save samples")
+    parser.add_option("--n_jobs", dest="n_jobs", type="int", default=8,
+                      help="Number of jobs")
     (options, _) = parser.parse_args() 
 
     file = options.input
@@ -126,17 +130,34 @@ def main():
     n = segmented_tiles.shape[0]
     s = segmented_tiles.shape[1]
     bins = np.zeros((n, s, s), dtype="uint8")
-    for i in trange(n):
+
+    def process_i(i):
         para = positions[i]
-        prob = segmented_tiles[i]
+        prob = segmented_tiles[i].copy()
         rgb = raw[i]
         list_img = post_process_out(prob, rgb)
-        bins[i] = list_img[0]
-        inp = list(para)
-        del inp[-2]
         if options.samples:
+            inp = list(para)
+            del inp[-2]
             for image, name in zip(list_img, out_names):
                 io.imsave(name.format(*inp), image, resolution=[1.0, 1.0])
+        return list_img[0]
+
+
+    labeled_bins = Parallel(n_jobs=options.n_jobs)(delayed(process_i)(i) for i in trange(n))
+    bins = np.stack(labeled_bins)
+
+    # for i in trange(n):
+    #     para = positions[i]
+    #     prob = segmented_tiles[i]
+    #     rgb = raw[i]
+    #     list_img = post_process_out(prob, rgb)
+    #     bins[i] = list_img[0]
+    #     inp = list(para)
+    #     del inp[-2]
+    #     if options.samples:
+    #         for image, name in zip(list_img, out_names):
+    #             io.imsave(name.format(*inp), image, resolution=[1.0, 1.0])
     np.savez("segmented_tiles_and_bins.npz", tiles=segmented_tiles, positions=positions,
             raw=raw, bins=bins)
 
